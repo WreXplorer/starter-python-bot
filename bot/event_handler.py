36ce35c2c12b2d9bb963e435a21170b3b351ca07
WreXplorer <b1cook@vianet.ca>
2016-05-27 00:55:42 -0400

    Update event_handler.py

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -26,6 +26,9 @@ class RtmEventHandler(object):
         elif event_type == 'channel_joined':
             # you joined a channel
             self.msg_writer.write_help_message(event['channel'])
+        elif event_type == 'im_created':
+            # you joined a private chat
+            self.msg_writer.write_help_message(event['channel'])
         elif event_type == 'group_joined':
             # you joined a private group
             self.msg_writer.write_help_message(event['channel'])
