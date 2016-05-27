cc77332144020554a63c569cf337ab23c37904e3
WreXplorer <b1cook@vianet.ca>
2016-05-27 01:03:34 -0400

    Update slack_bot.py

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -5,6 +5,7 @@ import traceback
 from slack_clients import SlackClients
 from messenger import Messenger
 from event_handler import RtmEventHandler
+from list_manager import ListManager
 
 logger = logging.getLogger(__name__)
 
