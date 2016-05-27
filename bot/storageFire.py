13aa31d9ee41530af1e87f3eb343eec9473dc7e6
WreXplorer <b1cook@vianet.ca>
2016-05-27 15:36:52 -0400

    Update storageFire.py

--- bot/storageFire.py
+++ bot/storageFire.py
@@ -3,12 +3,12 @@ from firebase import firebase
 FIREBASE_URL = "https://staples-bot.firebaseio.com/"
 
 class Storage(self, arg):
-    def save(self, arg):
+    def saveit(self, arg):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
         data = arg
         fb.put('/Testing/Node1', "Data", data) # Add data to Node Node1
     
-    def load(self):
+    def loadit(self):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
         result = fb.get('/Testing', "Node1") # Get  data from firebase
         return result
