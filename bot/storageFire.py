b708a17d0be8f0ad1cf2830924887e7ef4c6f6a6
WreXplorer <b1cook@vianet.ca>
2016-05-27 17:13:34 -0400

    Update storageFire.py

--- bot/storageFire.py
+++ bot/storageFire.py
@@ -5,7 +5,7 @@ FIREBASE_URL = "https://staples-bot.firebaseio.com/"
 class Storage:
     def saveit(self, data, value):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
-        fb.put('/Lists', data, value) # Add data to Node Node1
+        fb.put('/Lists/Todo', data, value) # Add data to Node Node1
     def loadit(self):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
         results = fb.get('/Lists/Todo', "1") # Get  data from firebase
