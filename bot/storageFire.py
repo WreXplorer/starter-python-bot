f7d5bb4c4af13e09b486a3d9198c879460ab334a
WreXplorer <b1cook@vianet.ca>
2016-05-27 15:55:10 -0400

    Update storageFire.py

--- bot/storageFire.py
+++ bot/storageFire.py
@@ -9,5 +9,6 @@ class Storage:
         fb.put('/Testing/Node1', "Data", data) # Add data to Node Node1
     def loadit(self):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
-        result = fb.get('/Testing', "Node1") # Get  data from firebase
-        return ("FB Data = %s" % result)
+        result = str(fb.get('/Testing', "")).replace("@staplesbot", "") # Get  data from firebase
+        
+        return result
