7400b16aef28609f7002b05784471d74c8ff4070
WreXplorer <b1cook@vianet.ca>
2016-05-27 17:37:44 -0400

    Update storageFire.py

--- bot/storageFire.py
+++ bot/storageFire.py
@@ -8,6 +8,7 @@ class Storage:
         fb.put('/Lists/Todo', data, value) # Add data to Node Node1
     def loadit(self):
         fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
-        results = fb.get('/Lists/Todo', None) # Get  data from firebase
+        for x in fb.get('/Lists/Todo', None):
+            results[x] = fb.get('/Lists/Todo', str(x))
         
-        return results
+        return results[1]
