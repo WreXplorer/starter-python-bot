5128b4f00460d001f130b597e746a41da66f4601
WreXplorer <b1cook@vianet.ca>
2016-05-27 01:39:16 -0400

    Update event_handler.py

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -51,6 +51,8 @@ class RtmEventHandler(object):
                     self.msg_writer.write_joke(event['channel'])
                 elif 'test' in msg_txt:
                     self.msg_writer.run_test(event['channel'])
+                elif 'todo' in msg_txt:
+                    self.msg.writer.new_task(event['channel'], msg_txt)
                 elif 'attachment' in msg_txt:
                     self.msg_writer.demo_attachment(event['channel'])
                 else:
